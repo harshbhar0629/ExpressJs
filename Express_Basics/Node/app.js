725845d5ecbde312c6e1bd4ceda403b58405411d
harshbhar0629 <harshbhar.0629@gmail.com>
2024-06-07 13:57:43 +0530

    Learn about ES module and common js module:

--- Express_Basics/Node/app.js
+++
@@ -1,7 +0,0 @@
-const obj = require("./math");
-
-console.log(obj.PI);
-console.log(obj.g);
-console.log(obj.mul(8,3));
-console.log(obj.sum(8,9));
-console.log("Changes required");
--- Express_Basics/Node/untracked.js
+++
@@ -1,1 +0,0 @@
-console.log("Untracked Detected:");
