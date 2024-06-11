7d692febed85e4de983eb4477966a46e9c550656
harshbhar0629 <harshbhar.0629@gmail.com>
2024-06-11 11:02:37 +0530

    Learn about EJS

--- EJS/index.js
+++ EJS/index.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const app = express();
-
 const port = 5123;
+const path = require("path"); // it include path
+
+app.set("view engine", "ejs"); // it is used to set view == templates
+
+app.set("views", path.join(__dirname, "/views")); // this can set the path of view folder so that if we run from parent folder then it works fine because it can take absolute path of views folder
+
+
+// it can automatically find views folder jha s run kia h wha views ko find krega issue bhi  ayega if we run from parent of ejs folder
+
+app.get('/', (req, res) => {
+    res.render("home.ejs"); // in ejs we r not sending the response we actually rendering the response
+    // res.send("sending done");
+})
+
+
+
 app.listen(port, () => {
     console.log(`App listening at port ${port}`);
 });
