a7b49c30d1fed15ec3be5df1b2d0023faed11bee
harshbhar0629 <harshbhar.0629@gmail.com>
2024-07-06 13:29:42 +0530

    Add codes:

--- express_with_mongodb/index.js
+++ express_with_mongodb/index.js
@@ -19,7 +19,7 @@ async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/chatApp");
 }
 
-// Serber created
+// Server created
 app.listen(3000, () => {
     console.log("App listen at port 3000");
 });
